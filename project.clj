e0e795c2091d5f2ed6f228f3aa2559b1ca1b85c8
Mitch Dzugan <mitchdzugan@gmail.com>
2020-04-27 18:01:55 -0500

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mayu-node/lein-template "0.1.5"
+(defproject mayu-node/lein-template "0.1.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
